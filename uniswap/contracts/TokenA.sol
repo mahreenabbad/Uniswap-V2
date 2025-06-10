97eae4113593890597bf72f8a7340af965c52b34
Mahreen <117590543+mahreenabbad@users.noreply.github.com>
2025-06-10 22:18:17 +0500

    Update TokenA.sol

--- uniswap/contracts/TokenA.sol
+++ uniswap/contracts/TokenA.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.28;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+//token for use in dex
 contract MyTokenA is ERC20, Ownable {
     constructor(address initialOwner)
         ERC20("MyTokenA", "TA")
