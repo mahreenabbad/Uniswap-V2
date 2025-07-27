0711393b83056797918e56665a27cc40ea8fc933
Mahreen <117590543+mahreenabbad@users.noreply.github.com>
2025-07-27 08:48:43 +0500

    Update mint.js

--- uniswap/script/mint.js
+++ uniswap/script/mint.js
@@ -33,7 +33,7 @@ const WETH = new ethers.Contract(WETHContAddress, WETHJson.abi, signer);
 const recipient = "0x13A538AEa48B39b17915eCb001f5330131c36a1A";
 
 async function main() {
-   const amount = ethers.parseUnits("1000", 18); // 1000 tokens, 18 decimals
+   //const amount = ethers.parseUnits("1000", 18); // 1000 tokens, 18 decimals
 
    console.log(`Sending TokenA mint transaction...`);
   // const tx = await contractA.mint(recipient, amount);
